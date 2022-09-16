e8a0161eeb02f9fdbf9843e26bcb5eba127ed164
esinyenakai <113664262+esinyenakai@users.noreply.github.com>
2022-09-16 09:49:24 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+     printf("my name is amani !\n");
+     printf("this is my first class!\n");
+    return 0;
+
+}
